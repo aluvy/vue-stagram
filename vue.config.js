ec8c7b9826c3a66fc71c31093fdadee9cbc605fa
miok <86095532+aluvy@users.noreply.github.com>
2023-10-04 14:19:33 +0900

    favicon

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,13 @@ module.exports = defineConfig({
     name: 'vue-stagram',
     themeColor: '#4DBA87',
     msTileColor: '#000000',
+    icons: [
+      {"src":"./favicon.ico","type":"image/png"},
+      // {"src":"./img/icons/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+      // {"src":"./img/icons/android-chrome-512x512.png","sizes":"512x512","type":"image/png"},
+      // {"src":"./img/icons/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable"},
+      // {"src":"./img/icons/android-chrome-maskable-512x512.png","sizes":"512x512","type":"image/png","purpose":"maskable"}
+    ],
     workboxOptions: {
       exclude: [/\.map$/, /manifest\.json$/, 'index.html']  // 제외하고 싶은 파일 명 (array 자료형)
     }
