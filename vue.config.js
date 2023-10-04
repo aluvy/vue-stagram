5f140d25533c579cb203b94fe44abde5c13aa118
miok <86095532+aluvy@users.noreply.github.com>
2023-10-04 13:41:57 +0900

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-stagram/dist' : '/',
+  // assetsDir: './',
   pwa: {
     name: 'vue-stagram',
     themeColor: '#4DBA87',
